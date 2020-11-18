2c32bbdbb58e8e8a32318f0bcddf5a6277381ff0
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro.local>
2020-11-18 14:06:30 -0700

    pre-release commit

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
     presets: [
         ['@vue/app', {
             corejs: 3,
-            useBuiltIns: "usage", // or "entry"
+            useBuiltIns: "entry", // or "entry"
             targets: {
                 browsers: "> .25%, not dead",
             }
